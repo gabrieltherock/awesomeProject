abe1d715b435b6f11703957b554c583997d5aa91
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-10-03 23:31:13 -0300

    variáveis e ponteiros

--- hello.go
+++ hello.go
@@ -11,4 +11,13 @@ func main() {
 	var version = 1.1
 	fmt.Println("Esse programa está na versão", version)
 	fmt.Println("O version é uma variável do tipo", reflect.TypeOf(version))
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Mostrar logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	//& aponta pro endereço de uma variável
+	fmt.Scan(&comando)
+	fmt.Println("Vc escolheu o comando", comando)
 }
