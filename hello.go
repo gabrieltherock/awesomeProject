24f4ab7471ef2eb26122f75607cc0c5fb6ee8959
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-10-04 22:40:46 -0300

    requisições http, loop infinito, retorno de 2 variáveis em função e operador _

--- hello.go
+++ hello.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"reflect"
 )
 
 func main() {
-	falaOi()
-	mostraMenu()
-
 	//if comando == 1 {
 	//	fmt.Println("Monitorando...")
 	//} else if comando == 2 {
@@ -20,19 +18,45 @@ func main() {
 	//	fmt.Println("Comando inválido!")
 	//}
 
-	comando := leComando()
+	//O operador _ (identificador em branco) significa que vou ignorar o segundo retorno dessa função
+	nome, _ := devolveNomeEIdade()
+	fmt.Println(nome)
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Tô saindo...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando inválido!")
-		os.Exit(-1)
+	for {
+		falaOi()
+		mostraMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Tô saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando inválido!")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func devolveNomeEIdade() (string, int) {
+	return "Gabriel", 21
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://alura.com.br"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Deu bom. O site ", site, "carregou")
+	} else {
+		fmt.Println("O site ", site, "está com problemas e retornou o status", resp.StatusCode)
 	}
 }
 
