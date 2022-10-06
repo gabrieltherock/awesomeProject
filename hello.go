6967246be650c4463481c0b7d6430c75ef062431
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-10-05 23:22:10 -0300

    collections (arrays e slices, que são tipo listas), for e tempos

--- hello.go
+++ hello.go
@@ -5,7 +5,11 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"time"
 )
+
+const monitoramentos = 3
+const tempoEntreMonitoramentos = 5
 
 func main() {
 	//if comando == 1 {
@@ -21,6 +25,8 @@ func main() {
 	//O operador _ (identificador em branco) significa que vou ignorar o segundo retorno dessa função
 	nome, _ := devolveNomeEIdade()
 	fmt.Println(nome)
+
+	exibeNomes()
 
 	for {
 		falaOi()
@@ -49,15 +55,47 @@ func devolveNomeEIdade() (string, int) {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://alura.com.br"
+	sites := []string{"https://www.alura.com.br", "https://www.google.com.br", "https://www.b3.com.br"}
+
+	fmt.Println(sites)
+
+	//for i := 0; i < len(sites); i++ {
+	//	fmt.Println(sites[i])
+	//}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("estou passando na posição", i, "e lá tem o site", site)
+			testaSite(site)
+		}
+		time.Sleep(tempoEntreMonitoramentos * time.Second)
+		fmt.Println("")
+	}
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Deu bom. O site ", site, "carregou")
 	} else {
 		fmt.Println("O site ", site, "está com problemas e retornou o status", resp.StatusCode)
 	}
+}
+
+func exibeNomes() {
+	nomes := []string{"Gabriel", "Pedro", "Rocha"}
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("Meu slice tem", len(nomes), "itens")
+	fmt.Println("Esse slice tá com capacidade pra armazenar", cap(nomes), "itens")
+
+	nomes = append(nomes, "Jurema")
+
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("Meu slice tem", len(nomes), "itens")
+	fmt.Println("Esse slice tá com capacidade pra armazenar", cap(nomes), "itens")
 }
 
 func falaOi() {
@@ -77,7 +115,10 @@ func mostraMenu() {
 func leComando() int {
 	var comandoLido int
 	//& aponta pro endereço de uma variável
-	fmt.Scan(&comandoLido)
+	_, err := fmt.Scan(&comandoLido)
+	if err != nil {
+		return 0
+	}
 	fmt.Println("Vc escolheu o comandoLido", comandoLido)
 
 	return comandoLido
